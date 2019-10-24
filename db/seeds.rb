58e7ffd9777169d66a86211682d8f68a3e364f52
Kevin Souto <soutokevin@gmail.com>
2019-10-23 22:27:39 -0300

    Fix seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,19 +6,21 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-require 'csv'
+require "csv"
 
-csv_text = File.read(Rails.root.join('db', 'pokemons.csv'))
-csv = CSV.parse(csv_text, :headers => true, :encoding => 'UTF-8')
+csv_text = File.read(Rails.root.join("db", "pokemons.csv"))
+csv = CSV.parse(csv_text, :headers => true, :encoding => "UTF-8")
 
 csv.each do |row|
   poké = Pokemon.new
-  poké.id = row['_id']
-  poké.name = row['name']
-  poké.image_url = row['image_url']
-  poké.identifier = row['identifier']
-  poké.weight = row['weight'].to_i
-  poké.height = row['height'].to_i
-  poké.kind = row['type']
+  poké.id = row["_id"]
+  poké.name = row["name"]
+  poké.image_url = row["image_url"]
+  poké.identifier = row["identifier"]
+  poké.weight = row["weight"].to_i
+  poké.height = row["height"].to_i
+  poké.kind = row["types"]
+
+  Pokemon.find(row["_id"]).destroy
   poké.save!
 end
