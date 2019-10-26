5ab1ff485ffe6417e768525c51b774abe02a7da3
Kevin Souto <soutokevin@gmail.com>
2019-10-26 15:11:31 -0300

    Update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :pokemons
-  resources :users
+  get "/pokemons", to: "pokemons#index"
+  get "/pokemons/:name", to: "pokemons#show"
+  get "/users/:username", to: "users#show"
+  post "/users", to: "users#create"
+  post "/users/:username/starred/:pokemon", to: "users#add_pokemon"
+  delete "/users/:username/starred/:pokemon", to: "users#add_pokemon"
 end
